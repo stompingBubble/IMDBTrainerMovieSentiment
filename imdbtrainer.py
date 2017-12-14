3f673f89a5c5b44d663cdd3e2db84264eb77a3d1
isa sand <isasandh@gmail.com>
2017-12-14 20:02:20 +0100

    some txt-files for testing. procent at 83.368 correct

--- imdbtrainer.py
+++ imdbtrainer.py
@@ -1,31 +1,47 @@
 import os
 import json
+import nltk
+from nltk import PorterStemmer
+from nltk.corpus import stopwords
+
+#nltk.download('stopwords')
 
 
 class IMDBTrainer():
-    def __init__(self, path="aclImdb/train"):
+    def __init__(self, path):#="aclImdb/train"):
         self.scores = []
         self.data = []
         self.size = 0
+        self.wrongReviews = []
         # Load up positive and negative reviews
+        
         for X in ["neg", "pos"]:
             for file in os.listdir( os.path.join( path, X ) ):
                 s=file.split("_")
                 score = int(s[1].replace(".txt",""))
                 if X=="neg":
-                    self.scores.append( -1 )
+                    self.scores.append( - 1)
                 else:
                     self.scores.append(  1 )
                 # read the review..
                 data = open( os.path.join( path, X, file ), encoding="utf-8").read()
+               
                 self.data.append( data )
+                
                 self.size += 1
                 
     def train( self, sentiment ):
         for i in range(self.size):
             sentiment.addStringScore(self.data[i], self.scores[i])
-        
                 
+    def writeToFile(self):
+        F = open("wrongReviews.txt", "w")
+        for i in range(len(self.wrongReviews)):
+            F.write(str(self.wrongReviews[i].encode('utf8')))
+            F.write("\n")
+            F.write("\n")
+        F.close()
+        
     def test( self, sentiment ):
         count = 0
         correct=0
@@ -40,14 +56,17 @@ class IMDBTrainer():
                     correct += 1
                 else:
                     wrong += 1
+                    
             elif ( s > 0.01 ):
                 if self.scores[i] > 0:
                     correct += 1
                 else:
                     wrong += 1
+
             else:
                 uncertain += 1
-        
+                
+        #self.writeToFile()
         print("ಠ_ಠ")
         print("Correct: {:.3f}%".format( 100*correct/count ) )
         print("Wrong: {:.3f}%".format( 100*wrong/count ) )
--- sentimentstore.py
+++ sentimentstore.py
@@ -1,4 +1,14 @@
+#some globals: 
+#best is min_lenght = 6 and num_of_words = 2
 
+
+MIN_LENGHT_OF_STRING = 6
+NUM_OF_WORDS_TO_PASS = 3
+
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+#nltk.download('stopwords')
 class SentimentStore:
     def __init__(self):
         self.store = {}
@@ -6,9 +16,36 @@ class SentimentStore:
         self.pos = 0
         self.neg = 0
         self.totalwordcount = 0
+        self.totalwordpaircount = 0
         self.wordcount = 0
-        self.sentiment_dict = {}
+        self.stop = self.loadStopWords()
+        self.words_store = {} 
         
+    def loadStopWords(self): 
+        with open('stopwords.txt', 'r') as myfile:
+            data=myfile.read()
+        return data
+    
+    def addSoloWordScore(self, word, score):
+        self.totalwordcount += 1
+        if word not in self.words_store.keys(): 
+            self.words_store[word] = score
+            self.timesseen[word] = 1
+            self.wordcount +=1
+            if score >= 1: 
+                self.pos += 1
+            elif score <1: 
+                self.neg += 1
+        else: 
+            self.words_store[word]+=score
+            self.timesseen[word]+=1
+    
+    def printStopWords(self):
+        print(self.stop)
+        
+    def getTotalWordPairCount(self):
+        return self.totalwordpaircount 
+    
     def getNumberOfWords(self):
         return self.wordcount
 
@@ -22,42 +59,48 @@ class SentimentStore:
         return self.totalwordcount
 
     def addWordScore(self, word, score):
-        self.totalwordcount += 1
+        self.totalwordpaircount += 1
         if word not in self.store.keys(): 
             self.store[word] = score 
             self.timesseen[word]=1
             self.wordcount += 1
-            if score == 1 :
+            if score >=1 :
                 self.pos += 1
-            elif score == -1:
+            elif score <1:
                 self.neg += 1
         else: 
             self.store[word]+= score
             self.timesseen[word]+=1
-                
         return
         
-    def addStringScore(self, string, score):
-        N = 1
-        string = string.replace("<br", "").replace("/>", "").replace(")", "").replace("(", "").replace("}","")
-       
-    
-        words = string.split(" ")
+    def addStringScore(self, s, score):
+        N = NUM_OF_WORDS_TO_PASS - 1
+        s = s.replace("#", "").replace("!", "").replace("-", "").replace("/", "").replace("<br", "").replace(":", "").replace("/>", "").replace(")", "").replace("(", "").replace('"', "").replace("}","").replace(".", "").replace("?", "").replace(",","").replace("\\", "").replace(":", "")
+        
+        #s = s.replace("1", "").replace("2", "").replace("3", "").replace("4", "").replace("5", "").replace("6", "").replace("7", "").replace("8", "").replace("9", "").replace("0", "")
+        words = s.split(" ")
         
         for i in range(N,len(words)):
             strang = ""
-            for wrd in words[i-N:i+1]:
-                strang += " " + wrd
-            if len(strang) > 3:
-                self.addWordScore(strang[1:], score)
+                
+            if len(words[i]) >0:
+                self.addSoloWordScore(words[i], score)
+                for wrd in words[i-N:i+1]:
+                    strang += " " + wrd
+                    
+                if len(strang) > MIN_LENGHT_OF_STRING: 
+                    strang = strang.lower()
+                    self.addWordScore(strang[1:], score)
         
     def getWordSentiment(self, word):
-        if word in self.store.keys():
+        if word in self.store.keys() :
             return self.store[word]
+        elif word in self.words_store.keys():
+            return self.words_store[word]
         return 0
     
     def getWordCount(self, word):
-        if word in self.store.keys(): 
+        if word in self.store.keys() or word in self.words_store.keys(): 
             return self.timesseen[word]
         return 0
 
@@ -69,25 +112,37 @@ class SentimentStore:
             return self.getWordSentiment(word) / self.getWordCount(word)
         else:
             return 0
-
-
-    def getStringSentiment(self, s):
-        N = 1
-        
-        s = s.replace("<br", "").replace("/>", "").replace(")", "").replace("(", "").replace("}","")
+                
+    def getStringSentiment(self, string):
+        N = NUM_OF_WORDS_TO_PASS - 1
+        string = string.replace("#", "").replace("-", "").replace("/", "").replace("<br", "").replace(":", "").replace("/>", "").replace(")", "").replace("(", "").replace('"', "").replace("}","").replace(".", "").replace("?", "").replace(",","").replace("!", "").replace("\\", "").replace(":", "")
+                                       
+        #string = string.replace("1", "").replace("2", "").replace("3", "").replace("4", "").replace("5", "").replace("6", "").replace("7", "").replace("8", "").replace("9", "").replace("0", "")
         score = 0
         count = 0
-        words = s.split(" ")
-        
+        words = string.split(" ")
         for i in range(N, len(words)):
             strang = ""
-            for wrd in words[i-N:i+1]:
-                strang += " " + wrd
-            
-            if len(strang) > 3:
-                count += 1
-                strang = strang.lower()
-                score += self.getNormalizedWordSentiment(strang[1:])
-            
+            #if len(words[i])> 3: 
+             #   score += self.getNormalizedWordSentiment(words[i])
+             #this increased to 83.204%?? dont know if it is right
+             
+            #if len(words[i]) > 3:
+            if len(words[i]) > 0:
+                
+                for wrd in words[i-N:i+1]:
+                    strang += " " + wrd
+                    
+                    
+                if len(strang) > MIN_LENGHT_OF_STRING: 
+                    count += 1
+                    strang = strang.lower()
+                    if self.getNormalizedWordSentiment(strang[1:]) == 0: 
+                        score += self.getNormalizedWordSentiment(words[i])
+                    else: 
+                        score += self.getNormalizedWordSentiment(strang[1:]) 
+                
+        if score == 0: 
+            return 0
         
         return score / count 
